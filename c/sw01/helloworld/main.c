e35d481913edec8902d0d858d0434e270c21ab2a
U-Daniel-NB\Daniel <Daniel@Daniel-NB.(none)>
2014-02-23 16:36:47 +0100

    Hello World in Netbeans

---
+++ c/sw01/helloworld/main.c
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.c
+ * Author: Daniel
+ *
+ * Created on 22. Februar 2014, 12:56
+ */
+
+#include <stdio.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Hello World");
+    return (0);
+}
+
