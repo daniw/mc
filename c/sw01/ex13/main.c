f7cc479993c96bb312f17aff67c0d8467e39bad4
U-Daniel-NB\Daniel <Daniel@Daniel-NB.(none)>
2014-02-23 18:07:31 +0100

    Klammern korrigiert

--- c/sw01/ex13/main.c
+++ c/sw01/ex13/main.c
@@ -12,29 +12,31 @@
  * @author daniw
  * @param wert Zahl, von welcher die binäre Repräsentation ausgegeben wird. 
  */
-void printbin(int wert) {
+void printbin(int wert)
+{
     unsigned int mask;
-    
-    mask = (1 << (sizeof(wert) * 8 - 1));
-    for (mask = (1 << (sizeof(wert) * 8 - 1)); mask >= 1; mask = mask >> 1)
+
+    mask = (1 << (sizeof (wert) * 8 - 1));
+    for (mask = (1 << (sizeof (wert) * 8 - 1)); mask >= 1; mask = mask >> 1)
     {
         putchar((wert & mask) > 0 ? '1' : '0');
     }
-    
+
     return;
 }
 
 /*
  * @author daniw
  */
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
     int wert;
-    
+
     printf("Wert als Integer (auch negativ): ");
     scanf("%d", &wert);
-    
+
     printbin(wert);
-    
+
     return (0);
 }
 
