2efbf7c16d085ed9a5bb80b766aebb752508ff3e
daniw <dani.winz@bluewin.ch>
2014-02-24 23:08:12 +0100

    wordcount

---
+++ c/sw01/wordcount/main.c
@@ -0,0 +1,52 @@
+/* 
+ * File:   main.c
+ * Author: daniw
+ *
+ * Created on 24. Februar 2014, 22:49
+ */
+
+/*
+ * Includes
+ */
+#include <stdio.h>
+
+/*
+ * Constants
+ */
+#define FALSE 0
+#define TRUE  1
+/*
+ * Function prototypes
+ */
+
+/*
+ * @author daniw
+ */
+int main(int argc, char** argv)
+{
+    char ch;
+    unsigned int words;
+    char space;
+    
+    do
+    {
+        ch = getchar();
+        if (ch == ' ' || ch == '\t' || ch == '\n')
+        {
+            space = TRUE;
+        }
+        else
+        {
+            if (space == TRUE)
+            {
+                words++;
+            }
+            space = FALSE;
+        }
+    }
+    while (ch != EOF);
+    printf("Number of Words: %d", words);
+    
+    return (0);
+}
+
