29ce5c9982ad412d032e3b07ac7e6c0c20ee5ae9
daniw <dani.winz@bluewin.ch>
2014-02-24 22:17:13 +0100

    kleine Korrekturen

--- mc/sw01/division.asm
+++ mc/sw01/division.asm
@@ -15,7 +15,6 @@ Start:
 	JMP End
 Division:			# Else
 	CLR					# Clear Error Flag
-	DEC
 	STA Error
 	LDA Dividend		# Division
 	DIV Divisor
@@ -23,7 +22,7 @@ Division:			# Else
 	LDA Dividend		# Rest
 	MOD Divisor
 	STA Quotient
-End:
+End:				# Stop
 	JMP End
 
 
@@ -32,17 +31,16 @@ End:
 02h	CLR
 03h	DEC
 04h	STA 16h
-05h	JMP 0Fh
+05h	JMP 0Eh
 06h	CLR
-07h	DEC
-08h	STA 16h
-09h	LDA 12h
-0Ah	DIV 13h
-0Bh	STA 14h
-0Ch	LDA 12h
-0Dh	MOD 13h
-0Eh	STA 14h
-0Fh	JMP 0Fh
+07h	STA 16h
+08h	LDA 12h
+09h	DIV 13h
+0Ah	STA 14h
+0Bh	LDA 12h
+0Ch	MOD 13h
+0Dh	STA 15h
+0Eh	JMP 0Eh
 
 
 
