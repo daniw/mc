bb35f8ece6ae150eabde312fa267e48bb266e101
daniw <dani.winz@bluewin.ch>
2014-03-03 22:07:25 +0100

    Script for downloading datasheets

---
+++ mc/datasheets/datasheets.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+### Power supply ###
+
+# Lithium battery controller
+wget -nc http://ww1.microchip.com/downloads/en/DeviceDoc/21984F.pdf
+# LDO
+wget -nc http://www.onsemi.com/pub_link/Collateral/NCP605-D.PDF
+# Current amplifier
+wget -nc http://diodes.com/datasheets/ZXCT1022.pdf
