92eb926d9d276e3197a26b4fb27fcecc0063c8de
daniw <dani.winz@bluewin.ch>
2014-03-06 18:01:50 +0100

    MCU

--- mc/datasheets/datasheets.sh
+++ mc/datasheets/datasheets.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+### MCU ###
+
+# Datasheet
+wget http://cache.freescale.com/files/microcontrollers/doc/data_sheet/MC9S08JM60.pdf
+
+# HCS08 Family reference manual
+wget http://cache.freescale.com/files/microcontrollers/doc/ref_manual/HCS08RMV1.pdf
+
+
 ### Power supply ###
 
 # Lithium battery controller
@@ -8,3 +17,5 @@ wget -nc http://ww1.microchip.com/downloads/en/DeviceDoc/21984F.pdf
 wget -nc http://www.onsemi.com/pub_link/Collateral/NCP605-D.PDF
 # Current amplifier
 wget -nc http://diodes.com/datasheets/ZXCT1022.pdf
+
+
