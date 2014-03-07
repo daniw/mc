1347ebdbcdb7fb3b6cc856ec7be1f03ae6f41c5b
daniw <dani.winz@bluewin.ch>
2014-03-07 12:36:28 +0100

    Bluetooth module

--- mc/datasheets/datasheets.sh
+++ mc/datasheets/datasheets.sh
@@ -3,10 +3,10 @@
 ### MCU ###
 
 # Datasheet
-wget http://cache.freescale.com/files/microcontrollers/doc/data_sheet/MC9S08JM60.pdf
+wget -nc http://cache.freescale.com/files/microcontrollers/doc/data_sheet/MC9S08JM60.pdf
 
 # HCS08 Family reference manual
-wget http://cache.freescale.com/files/microcontrollers/doc/ref_manual/HCS08RMV1.pdf
+wget -nc http://cache.freescale.com/files/microcontrollers/doc/ref_manual/HCS08RMV1.pdf
 
 
 ### Power supply ###
@@ -19,3 +19,5 @@ wget -nc http://www.onsemi.com/pub_link/Collateral/NCP605-D.PDF
 wget -nc http://diodes.com/datasheets/ZXCT1022.pdf
 
 
+### Bluetooth module ###
+wget -nc http://www.e-gizmo.com/KIT/images/EGBT-04/EGBT-045MS-046S%20Bluetooth%20Module%20Manual%20rev%201r0.pdf
