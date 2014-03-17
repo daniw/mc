0e1908dc28ccc83e731c867baf06455c84d31bb1
daniw <dani.winz@bluewin.ch>
2014-03-18 00:25:14 +0100

    Ueb 3_2

---
+++ mc/sw04/ueb3_2/FLASH/Sources/main_asm.d
@@ -0,0 +1,5 @@
+
+../Sources/main.o: \
+	../Sources/main.asm \
+	C:/Daten/Daniel/studium/git_repo/sem4/mc/mc/sw04/ueb3_2/Project_Headers/derivative.inc \
+	C:/Daten/Daniel/studium/git_repo/sem4/mc/mc/sw04/ueb3_2/Project_Headers/MC9S08JM60.inc 
---
+++ mc/sw04/ueb3_2/Sources/main.asm
@@ -0,0 +1,81 @@
+;--------------------------------------------------------------------
+;          HSLU T&A Hochschule Luzern Technik+Architektur            
+;--------------------------------------------------------------------
+;
+; \brief         Template for HCS08 Assembler
+; \file
+; \author        J.Wassner/C.Jost/L.Kressebuch
+; \date          10.10.2012
+;
+; \b Language:   Assembler \n\n
+; \b Target:     JM60 Testboard V1.1 \n
+;
+; \par Description:
+;
+; $Id: main.asm 485 2013-09-22 06:02:55Z zajost $
+;--------------------------------------------------------------------
+                include 'derivative.inc'
+                
+
+; Global definierte Symbole sind von aussen sichtbar.
+;--------------------------------------------------------------------
+                GLOBAL      _Startup    ; Start Hauptprogramm
+                GLOBAL      main
+                GLOBAL      dummy       ; Dummy Interrupt Service Routine
+
+; EQUATIONS
+;--------------------------------------------------------------------
+StackSize:      EQU     $60             ; Equation for the stack size
+pi:             EQU     31416           ; Example for a constant value
+
+; STACK, VARIABLEN (Datenspeicher) und KONSTANTEN (Festwertspeicher) 
+;--------------------------------------------------------------------
+DATEN_STACK:    SECTION
+TofStack:       DS      StackSize-1     ; definiton of "Top of Stack"
+BofStack:       DS      1               ; definition of "Bottom of Stack"
+
+DATEN:          SECTION
+Variable1:      DS      1               ; Example of a 1 Byte Variable
+Array1:         DS      $20             ; Example of an Array of $20 Bytes
+
+KONSTANTEN:     SECTION
+Maske1:         DC.B    %00000001
+Parameter1:     DC.B    $3A             ; DC mit Punkt-> Coloring geht nicht!
+Parameter2:     DC.W    57100
+Reserve_Par:    DS      16              ; nur leerer Bereich reservieren!
+STRING1:        DC.B    10,"Hello",$0D
+
+; Programmstart (Initialisierungen)
+;--------------------------------------------------------------------
+PROGRAMM:       SECTION                 ; Code Segment
+_Startup:                               ; Resetvektor zeigt hierhin
+Stackinit:      LDHX    #(BofStack+1)
+                TXS                     ; TXS dekrementiert, deshalb oben +1     
+                LDA     #$00
+                STA     SOPT1           ; Disable Watchdog
+
+; Start des Benutzer-Codes
+;--------------------------------------------------------------------
+main:       
+
+                LDX   #$C1				; Load $C1 to X register
+Loop:           						; repeat
+                LDA   ,X				; 	Load  data from where X register points to
+                EOR   $C0				; 	Compare loaded data with content of 0xC0
+                STA   $102B				; 	Store differences in 0x102B
+                INCX					; 	increment Pointer X
+                BNE   Loop				; until bytes are equal
+
+EndLoop:        BRA     *               ; Endlos-Loop (=Programmende falls weiter oben Loop vergessen wurde)
+                
+
+
+
+
+;--------------------------------------------------------------------
+; dummy - Alle nicht benutzten Interruptvektoren werden hierher
+;         geleitet damit kein Absturz passiert, wenn ein unerwarteter
+;         Interrupt auftritt. (z.B. loss of lock vom ICG)
+;--------------------------------------------------------------------
+dummy:          BGND
+                BRA     dummy
