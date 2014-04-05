244155b631568185d0e458c7dca575da2998a0ed
daniw <dani.winz@bluewin.ch>
2014-04-05 21:41:25 +0200

    Initial commit for addressmanager project

---
+++ c/sw06/addressmanager/main.c
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.c
+ * Author: daniw
+ *
+ * Created on April 5, 2014, 9:30 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "ui.h"
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+    printhelp();
+    
+    return (EXIT_SUCCESS);
+}
+
---
+++ c/sw06/addressmanager/ui.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   ui.h
+ * Author: daniw
+ *
+ * Created on April 5, 2014, 9:32 PM
+ */
+
+#ifndef UI_H
+#define	UI_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+    
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* UI_H */
+
