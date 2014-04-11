db1dbf1ffdc56b2d11805d5ab30abaec70f6a404
daniw <dani.winz@bluewin.ch>
2014-04-11 07:32:17 +0200

    Exercise 6 without CPU interaction

---
+++ mc/sw07/Ueb6_3/Sources/main.c
@@ -0,0 +1,60 @@
+/**
+ *--------------------------------------------------------------------\n
+ *          HSLU T&A Hochschule Luzern Technik+Architektur            \n
+ *--------------------------------------------------------------------\n
+ *
+ * \brief         C Template for the MC Car
+ * \file
+ * \author        Daniel Winz, Adrian Imboden
+ * \date          06.03.2014
+ *
+ * \b Language:   Ansi-C \n\n
+ * \b Target:     MC-Car  \n
+ *
+ * \par Description:
+ *
+ * $Id: main.c 485 2014-06-03 21:17:55Z daniw $
+ *--------------------------------------------------------------------
+ */
+#include "platform.h" /* include peripheral declarations */
+
+#define CLOCK 1000000
+#define FREQ_LED 2
+#define PRESC_MASK 0x07
+#define PRESC_VAL (1<<PRESC_MASK)
+#define MOD_VALUE ((CLOCK / PRESC_VAL / FREQ_LED) - 1)
+#if MOD_VALUE > 0xffff
+    #error "Modulo Register out of range -> Check Prescaler and Frequency values"
+#endif
+
+#define LED 0x04
+
+void InitTimer(void)
+{
+    TPM1SC_PS = PRESC_MASK; // Prescaler 128
+    TPM1MOD = MOD_VALUE;    // Modulo value according to LED Frequency
+    TPM1SC_CLKSx = 2;       // Fixed system clock
+
+    TPM1C4V = 0x0001;       // Toggle LED on compare value
+    TPM1C4SC_MS4x = 1;      //
+    TPM1C4SC_ELS4x = 1;     //
+}
+
+/**
+ * main program
+ */
+void main(void)
+{
+    EnableInterrupts;       // Interrupts aktivieren
+
+    PTFDD = LED;
+
+    InitTimer();
+
+    for(;;)
+    {
+        __RESET_WATCHDOG();  /* feeds the dog */
+    }
+
+  /* please make sure that you never leave main */
+}
